c4e040e144f77a41f78364797dceb98570589e19
Serhan Ertürk <srnerturk@gmail.com>
2019-12-24 10:49:26 +0300

    article added

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ function App() {
         { date: "14 Ağustos 2019", text: "o ilk an", image: cdnUrl + "1.jpg", title: "Herşeye Merhaba!" },
         { date: "15 Ağustos 2019", text: "ilk like'ı atma çabalaları..", image: cdnUrl + "2.jpg", title: "İlk Like!" },
         { date: "15 Ağustos 2019", text: "Parantez Severiz!", image: cdnUrl + "3.jpg", title: "İlk Parantez!" },
-        { date: "16 Ağustos 2019", text: "Utangaç çocuk numarayı istiyor, kızın paketi bitmiş yazık, püüü!", image: cdnUrl + "4.jpg", title: "Burası kasıyor Whatsapp var mı?" }
-
+        { date: "16 Ağustos 2019", text: "Utangaç çocuk numarayı istiyor, kızın paketi bitmiş yazık, püüü!", image: cdnUrl + "4.jpg", title: "Burası kasıyor Whatsapp var mı?" },
+        { date: "16 Ağustos 2019", text: "nihayet whatsapptan yazdı, oh be", image: cdnUrl + "5.jpg", title: "Merhaba wassapçığım" },
+        { date: "16 Ağustos 2019", text: "ilişkimizdeki sanatsal çalışmalara başladık", image: cdnUrl + "6.jpg", title: "Sanatı seviyor ve takdir ediyoruz" }
     ]
     return (
         <div id="timeline-content">
